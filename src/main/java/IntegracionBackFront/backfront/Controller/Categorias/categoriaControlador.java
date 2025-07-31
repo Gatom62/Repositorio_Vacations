be680b6ec2421ca2169c63f9246e973e050e5dbf
Gatom62 <20240046@ricaldone.edu.sv>
2025-07-31 15:31:08 -0600

    categoria enpaginada

--- src/main/java/IntegracionBackFront/backfront/Controller/Categorias/categoriaControlador.java
+++ src/main/java/IntegracionBackFront/backfront/Controller/Categorias/categoriaControlador.java
@@ -5,13 +5,11 @@ import IntegracionBackFront.backfront.Services.Categories.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/category")
 public class categoriaControlador {
