707f25588295aac5585d220e84e6df00a82409f4
Gatom62 <20240046@ricaldone.edu.sv>
2025-07-31 15:06:14 -0600

    categoria enpaginada

---
+++ src/main/java/IntegracionBackFront/backfront/Controller/Categorias/categoriaControlador.java
@@ -0,0 +1,49 @@
+package IntegracionBackFront.backfront.Controller.Categorias;
+
+import IntegracionBackFront.backfront.Models.DTO.Categories.CategoryDTO;
+import IntegracionBackFront.backfront.Services.Categories.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/category")
+public class categoriaControlador {
+
+    //Inyectamos el sevice osea la clase
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping("/getDataCategorias")
+    private ResponseEntity<Page<CategoryDTO>> optenerDatos(
+            @RequestParam(defaultValue = "0") int page, //La Api va a devolver paginas, pero 10 registro por paginas y minimo 0
+            @RequestParam(defaultValue = "3") int size
+    ){
+        //parte 1. Se evalua cuantos registros quiere ver el usuario por pagina
+        //Validacion para que el usuario seleccione una cantidad de datos correcta
+        if (size <= 0 || size > 50) {
+            ResponseEntity.badRequest().body(Map.of(
+                    "status", "Ek tamaño de la agina deve de estar entre 1 y 50"
+            ));
+            return ResponseEntity.ok(null);
+        }
+
+        Page<CategoryDTO> category = service.getAllCategories(page, size);
+
+        //parte 2. Invocando al metodo getAllCategories contenido en el service
+        //Evaluamos si la API nos tira datos, y si no hay registros, entonces no deci este mensaje
+        if (category == null) {
+            ResponseEntity.badRequest().body(Map.of(
+                    "status", "No hay categorias registradas"
+            ));
+        }
+
+        return ResponseEntity.ok(category);
+    }
+}
